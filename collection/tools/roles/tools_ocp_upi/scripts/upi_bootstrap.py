d03b45edf9d9b39cab86803b6347c7fac1b6a566
Itay Matza <imatza@redhat.com>
2024-03-28 14:18:36 +0200

    Support UPI installation in the shiftstack-qa automation
    https: //issues.redhat.com/browse/OSPRH-4173
    Change-Id: I04901c5ec04662dbf3cf83ab959ba13110384f7c

---
+++ collection/tools/roles/tools_ocp_upi/scripts/upi_bootstrap.py
@@ -0,0 +1,41 @@
+import base64
+import json
+import os
+
+with open('bootstrap.ign', 'r') as f:
+    ignition = json.load(f)
+
+storage = ignition.get('storage', {})
+files = storage.get('files', [])
+
+infra_id = os.environ.get('INFRA_ID', 'openshift').encode()
+hostname_b64 = base64.standard_b64encode(infra_id + b'-bootstrap\n').decode().strip()
+files.append(
+{
+    'path': '/etc/hostname',
+    'mode': 420,
+    'contents': {
+        'source': 'data:text/plain;charset=utf-8;base64,' + hostname_b64,
+    },
+})
+
+ca_cert_path = os.environ.get('OS_CACERT', '')
+if ca_cert_path:
+    with open(ca_cert_path, 'r') as f:
+        ca_cert = f.read().encode()
+        ca_cert_b64 = base64.standard_b64encode(ca_cert).decode().strip()
+
+    files.append(
+    {
+        'path': '/opt/openshift/tls/cloud-ca-cert.pem',
+        'mode': 420,
+        'contents': {
+            'source': 'data:text/plain;charset=utf-8;base64,' + ca_cert_b64,
+        },
+    })
+
+storage['files'] = files
+ignition['storage'] = storage
+
+with open('bootstrap.ign', 'w') as f:
+    json.dump(ignition, f)
