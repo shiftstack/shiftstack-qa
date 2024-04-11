7633a019e985fe11715e3d9f732a7b243cce9f6b
Ramón Lobillo <rlobillo@redhat.com>
2024-04-11 14:02:04 +0200

    add gate.sh
    Change-Id: Iec6ee21ce245d49f4d7193f1abd2e7ae64114700

---
+++ gate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo 1. Re-create virtualenv
+rm -rf ~/venv_precommit
+python -m venv ~/venv_precommit
+. ~/venv_precommit/bin/activate
+pip install pre-commit 
+pre-commit install
+pre-commit clean
+echo
+echo 2. Clone patch
+git clone https://review.gerrithub.io/shiftstack/shiftstack-qa ~/venv_precommit/shiftstack-qa/
+cd ~/venv_precommit/shiftstack-qa/
+git fetch https://review.gerrithub.io/shiftstack/shiftstack-qa $GERRIT_REFSPEC
+echo
+echo 3. Run pre-commit
+ansible-galaxy install -r requirements.yaml
+echo $VAULT > 'xx'
+ANSIBLE_VAULT_PASSWORD_FILE='xx' pre-commit run
