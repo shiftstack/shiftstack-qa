5de13c7673147ac4218ae8fcd725c7215b3f73a1
Itay Matza <imatza@redhat.com>
2024-05-16 14:11:57 +0300

    [WIP] Add playbook execution to the gate
    Change-Id: Ib43534b50c01626de2a583de7e47516915fe6813

--- gate.sh
+++ gate.sh
@@ -1,7 +1,10 @@
-podman run -u root --pull always \
+podman run --pull always \
 	-v .:/home/cloud-admin/shiftstack-qa:Z  \
 	quay.io/shiftstack-qe/shiftstack-client \
 	/bin/bash -c 'cd shiftstack-qa && \
 	  ansible-galaxy install -r requirements.yaml && \
-	  ANSIBLE_VAULT_PASSWORD_FILE=/home/cloud-admin/.vault-pass pre-commit run'
+	  export ANSIBLE_VAULT_PASSWORD_FILE=/home/cloud-admin/.vault-pass && \
+	  pre-commit install && pre-commit run && \
+	  cd && \
+	  ansible-navigator run shiftstack-qa/playbooks/ocp_testing.yaml --extra-vars @shiftstack-qa/jobs_definitions/cifmw-gate.yaml -vvv'
 #EOF
